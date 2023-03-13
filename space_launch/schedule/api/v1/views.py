759171e86cf507bec2695f2cad00b678638c2d12
BulgakovMax <max.bulgakov.v@gmail.com>
2023-03-13 21:42:15 +0200

    Add scropt

--- space_launch/schedule/api/v1/views.py
+++ space_launch/schedule/api/v1/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from space_launch.schedule.models import Rocket
