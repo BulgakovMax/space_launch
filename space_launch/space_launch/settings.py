7e302e8d43f4718315dfa9332299f2aa8ad90d5e
BulgakovMax <max.bulgakov.v@gmail.com>
2023-04-06 23:04:39 +0300

    fix templates error

--- space_launch/space_launch/settings.py
+++ space_launch/space_launch/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'space_launch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
